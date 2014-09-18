2d6442b0c4a070bab3ff391fd5c1346a17456d85
Aleksandr Pavlenko <my.aleksandr.pavlenko@gmail.com>
2014-09-18 12:15:02 +0300

    App changes

--- src/main/java/net/pavlenko/App.java
+++ src/main/java/net/pavlenko/App.java
@@ -5,8 +5,7 @@ package net.pavlenko;
  *
  */
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println( "Hello!" );
     }
 }
